a9cf4382649eb031ddd1bb2b3105837181cd2a0f
Nikhilesh Kumar <nikhilesh2206@gmail.com>
2026-08-12 21:32:19 +0530

    remove cross origin

--- portfolio-service/src/main/java/com/nikhilesh/portfolio_service/controller/ContactController.java
+++ portfolio-service/src/main/java/com/nikhilesh/portfolio_service/controller/ContactController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/contact")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ContactController {
 
     private final ContactEmailService emailService;
